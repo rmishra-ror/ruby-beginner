9c06b7015d1dda6adfc9dc877e07a2546bcd1afa
rahul mishra <rahulismishra@gmail.com>
2017-03-11 10:20:01 +0530

    Initial pyramid Class structure

---
+++ pyramid.rb
@@ -0,0 +1,9 @@
+# Pyramid Class Conatins functions which return diffrent types of pattern of pyramid 
+class Pyramid
+  def left_align_star_pyramid(rows)
+  end
+  def right_align_star_pyramid(rows)
+  end
+  def center_align_star_pyramid(rows)
+  end
+end
