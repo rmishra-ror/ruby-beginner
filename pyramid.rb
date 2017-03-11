854151e76f3276270d352b97b6184c9be0afd347
rahul mishra <rahulismishra@gmail.com>
2017-03-11 10:39:37 +0530

    Implement left, right and center justify * Pyramid

--- pyramid.rb
+++ pyramid.rb
@@ -1,9 +1,33 @@
 # Pyramid Class Conatins functions which return diffrent types of pattern of pyramid 
 class Pyramid
+
   def left_align_star_pyramid(rows)
+    output_pattern = ''
+    (1..rows).each do |row|
+     output_pattern += '*' * row +"\n"
+    end
+    output_pattern.strip
   end
+
   def right_align_star_pyramid(rows)
+    output_pattern = ''
+    (1..rows).each do |row|
+     output_pattern += ('*' * row).rjust(rows) +"\n"
+    end
+    output_pattern.rstrip
   end
+
   def center_align_star_pyramid(rows)
+    output_pattern = ''
+    (1..rows).each do |row|
+     output_pattern += ('*' * row).center(rows) +"\n"
+    end
+    output_pattern
   end
 end
+
+puts Pyramid.new.left_align_star_pyramid(5)
+puts "------------------------------------"
+puts Pyramid.new.right_align_star_pyramid(5)
+puts "------------------------------------"
+puts Pyramid.new.center_align_star_pyramid(5)
