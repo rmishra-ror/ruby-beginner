e9e5ef1a2acd9e497ea6546bb17ffe01465473c0
rahul mishra <rahulismishra@gmail.com>
2017-03-11 11:17:47 +0530

    Refactor the code and use genralized def to return pyramid pattern

--- pyramid.rb
+++ pyramid.rb
@@ -1,33 +1,22 @@
-# Pyramid Class Conatins functions which return diffrent types of pattern of pyramid 
+# Pyramid Class Conatins functions which return diffrent types of patterns of pyramid
 class Pyramid
 
-  def left_align_star_pyramid(rows)
+  def pyramid(rows, align)
     output_pattern = ''
     (1..rows).each do |row|
-     output_pattern += '*' * row +"\n"
-    end
-    output_pattern.strip
-  end
-
-  def right_align_star_pyramid(rows)
-    output_pattern = ''
-    (1..rows).each do |row|
-     output_pattern += ('*' * row).rjust(rows) +"\n"
+     output_pattern += ('*' * row).send(align.to_sym, rows).rstrip() +"\n"
     end
     output_pattern.rstrip
   end
+  def left_align_star_pyramid(rows)
+    pyramid(rows, 'ljust')
+  end
+
+  def right_align_star_pyramid(rows)
+    pyramid(rows, 'rjust')
+  end
 
   def center_align_star_pyramid(rows)
-    output_pattern = ''
-    (1..rows).each do |row|
-     output_pattern += ('*' * row).center(rows) +"\n"
-    end
-    output_pattern
+    pyramid(rows, 'center')
   end
 end
-
-puts Pyramid.new.left_align_star_pyramid(5)
-puts "------------------------------------"
-puts Pyramid.new.right_align_star_pyramid(5)
-puts "------------------------------------"
-puts Pyramid.new.center_align_star_pyramid(5)
